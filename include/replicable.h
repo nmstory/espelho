474f5a1eb6f15c3b528f2b8c11817deadc0bebaa
Nathan <45177569+nmstory@users.noreply.github.com>
2026-06-14 23:01:16 +0100

    Replicable.h clean-up

--- include/replicable.h
+++ include/replicable.h
@@ -7,13 +7,6 @@ enum class TypeID : uint16_t {
 	Position = 1,
 };
 
-struct PositionReplicable;
-
-struct ReplicableVisitor {
-	virtual void visit(PositionReplicable&) = 0;
-	virtual ~ReplicableVisitor() = default;
-};
-
 class Replicable {
 public:
 	virtual ~Replicable() = default;
@@ -21,7 +14,6 @@ public:
 	virtual bool process(WriteStream& s) = 0;
 	virtual bool process(ReadStream& s)  = 0;
 	virtual TypeID typeID() const = 0;
-	virtual void accept(ReplicableVisitor&) = 0;
 
 	int id = 0;
 };

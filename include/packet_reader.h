a98eba2d8eb2c08ac28d6f3c856aaa50336ae04a
Nathan <45177569+nmstory@users.noreply.github.com>
2026-06-21 23:47:27 +0100

    Deserialise in-place

--- include/packet_reader.h
+++ include/packet_reader.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <stream.h>
 #include <replicable.h>
 #include <vector>
@@ -7,7 +8,8 @@
 
 struct PacketReader {
 public:
-    std::vector<std::unique_ptr<Replicable>> read(const uint8_t* data, size_t len, const TypeRegistry& registry)
+    std::vector<std::unique_ptr<Replicable>> read(const uint8_t* data, size_t len, const TypeRegistry& registry,
+                                                    std::vector<std::unique_ptr<Replicable>>& objects)
     {
         std::vector<std::unique_ptr<Replicable>> result;
         ReadStream stream{data, len};
@@ -17,13 +19,29 @@ public:
             TypeID tid;
             if (!process_pod(stream, tid)) break;
 
-            auto obj = registry.create(tid);
-            if (!obj) break;
+            
+            int id;
+            if (!process_pod(stream, id)) break;
+            
+            auto objItr = std::find_if(objects.begin(), objects.end(),
+            [&](const std::unique_ptr<Replicable>& obj) {
+                return obj->typeID() == tid && obj->id == id;
+            });
+            
+            if (objItr != objects.end())
+            {
+                if (!(*objItr)->process(stream)) break; // deserialise in-place
+            }
+            else
+            {
+                auto obj = registry.create(tid); // object not recognised, create new
+                if (!obj) break;
+                obj->id = id;
+                if (!obj->process(stream)) break;
+                
+                result.push_back(std::move(obj));
+            }
 
-            if (!process_pod(stream, obj->id)) break;
-            if (!obj->process(stream)) break;
-
-            result.push_back(std::move(obj));
         }
 
         return result;

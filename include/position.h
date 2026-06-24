df7d6438ee7efa1d39a54233dd5c400ce026220e
Nathan <45177569+nmstory@users.noreply.github.com>
2026-06-25 00:03:35 +0100

    GTest & CTest

--- include/position.h
+++ include/position.h
@@ -5,14 +5,14 @@
 
 struct Position : public Replicable {
 
-    Position(const int& x, const int& y) : m_X(x), m_Y(y) {}
+    Position(int x = 0, int y = 0) : m_X(x), m_Y(y) {}
 
     static std::unique_ptr<Replicable> make() {
         return std::make_unique<Position>();
     }
 
-    bool process(WriteStream& s) override { return process_pod(s, x) && process_pod(s, y); }
-    bool process(ReadStream& s)  override { return process_pod(s, x) && process_pod(s, y); }
+    bool process(WriteStream& s) override { return process_pod(s, m_X) && process_pod(s, m_Y); }
+    bool process(ReadStream& s)  override { return process_pod(s, m_X) && process_pod(s, m_Y); }
     TypeID typeID() const override { return TypeID::Position; }
 
 private:
